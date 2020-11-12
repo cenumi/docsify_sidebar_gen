8d0d6c38be6613b43a3db93e48b2dc247c62c92f
c1yde3 <c1yde3@outlook.com>
2020-11-12 12:20:58 +0800

    去除文件名的空格

--- lib/docsify_sidebar_gen.dart
+++ lib/docsify_sidebar_gen.dart
@@ -9,7 +9,7 @@ void readDirs() {
   var sb = StringBuffer();
   for (final f in files) {
     if (p.extension(f.path) == '.md' && p.basename(f.path) != '_sidebar.md') {
-      sb.writeln('- [${p.basenameWithoutExtension(f.path)}](${p.relative(f.path)})');
+      sb.writeln('- [${p.basenameWithoutExtension(f.path).trim()}](${p.relative(f.path)})');
     }
   }
   file.writeAsStringSync(sb.toString());
